980ab56bc259530a170f9bb0cf5ee9382e1a79e5
datasara005 <datasareton@gmail.com>
2022-11-28 21:45:16 +0400

    frag

--- app/src/main/java/com/example/fragmentebi/MainActivity.kt
+++ app/src/main/java/com/example/fragmentebi/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.fragmentebi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var gadasvla:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        gadasvla=findViewById(R.id.gadasvla)
+        gadasvla.setOnClickListener {
+            fragmentisshecvla(dasamatebeli_fragmenti())
+        }
+
+    }
+
+    private fun fragmentisshecvla(dasamatebeliFragmenti: dasamatebeli_fragmenti) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.charcho,dasamatebeliFragmenti)
+        fragmentTransaction.commit()
+
+
     }
 }
